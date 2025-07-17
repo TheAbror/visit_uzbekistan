d81c7f777a1053289d4bd6b3f6dd2d8b807417b5
TheAbror <oyjddkvj@gmail.com>
2025-07-17 11:49:35 +0500

    fixed cities tab loader

--- lib/features/root/presentation/tabs/cities_tab/cities_tab.dart
+++ lib/features/root/presentation/tabs/cities_tab/cities_tab.dart
@@ -5,75 +5,65 @@ class CitiesTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<CitiesTabBloc, CitiesTabState>(
-      builder: (context, state) {
-        if (state.blocProgress == BlocProgress.IS_LOADING) {
-          return Center(child: CircularProgressIndicator());
-        }
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        SingleChildScrollView(
+          child: BlocBuilder<CitiesTabBloc, CitiesTabState>(
+            builder: (context, state) {
+              if (state.blocProgress == BlocProgress.IS_LOADING) {
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: Center(child: CircularProgressIndicator()),
+                );
+              }
 
-        if (state.blocProgress == BlocProgress.FAILED) {
-          return const SomethingWentWrong();
-        }
+              if (state.blocProgress == BlocProgress.FAILED) {
+                return const SomethingWentWrong();
+              }
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 100.h),
+                child: Column(
+                  children: [
+                    CitiesTabSearchWidget(),
+                    state.citiesSearched.isEmpty
+                        ? Container(
+                            margin: EdgeInsets.only(top: 300.h),
+                            child: Center(
+                              child: Text('No results'),
+                            ),
+                          )
+                        : GridView.builder(
+                            shrinkWrap: true,
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
+                              mainAxisSpacing: 8.h,
+                              crossAxisSpacing: 8.h,
+                              childAspectRatio: 0.85,
+                            ),
+                            padding: EdgeInsets.symmetric(
+                                horizontal: defaultPadding),
+                            itemCount: state.citiesSearched.length,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              final singleItem = state.citiesSearched[index];
 
-        return Stack(
-          alignment: Alignment.topCenter,
-          children: [
-            SingleChildScrollView(
-              child: BlocBuilder<CitiesTabBloc, CitiesTabState>(
-                builder: (context, state) {
-                  if (state.blocProgress == BlocProgress.IS_LOADING) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-
-                  if (state.blocProgress == BlocProgress.FAILED) {
-                    return const SomethingWentWrong();
-                  }
-                  return Padding(
-                    padding: EdgeInsets.symmetric(vertical: 100.h),
-                    child: Column(
-                      children: [
-                        CitiesTabSearchWidget(),
-                        state.citiesSearched.isEmpty
-                            ? Container(
-                                margin: EdgeInsets.only(top: 300.h),
-                                child: Center(
-                                  child: Text('No results'),
-                                ),
-                              )
-                            : GridView.builder(
-                                shrinkWrap: true,
-                                gridDelegate:
-                                    SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2,
-                                  mainAxisSpacing: 8.h,
-                                  crossAxisSpacing: 8.h,
-                                  childAspectRatio: 0.85,
-                                ),
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: defaultPadding),
-                                itemCount: state.citiesSearched.length,
-                                physics: NeverScrollableScrollPhysics(),
-                                itemBuilder: (context, index) {
-                                  final singleItem =
-                                      state.citiesSearched[index];
-
-                                  return GridViewItem(item: singleItem);
-                                },
-                              ),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            ),
-            CustomAppBar(
-              title: 'Cities',
-              color: AppColors.primary,
-              height: 45.h,
-            ),
-          ],
-        );
-      },
+                              return GridViewItem(item: singleItem);
+                            },
+                          ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+        CustomAppBar(
+          title: 'Cities',
+          color: AppColors.primary,
+          height: 45.h,
+        ),
+      ],
     );
   }
 }
